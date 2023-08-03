5246b7c8af55fa2f273da433ce3ed4ec6b2df774
Mehmet <mehmetpektasss18@gmail.com>
2023-08-03 16:59:50 +0300

    Added new codes to generate .txt

--- secret_not.py
+++ secret_not.py
@@ -1,15 +1,30 @@
 import tkinter
-
+from tkinter import messagebox , END
 
 window = tkinter.Tk()
 window.title("Secret Notes")
 window.config(padx=30,pady=30)
 
-def button():
-    pass
+def save_and_encrypt_notes():
+    title = entry1.get()
+    message = low_input.get("1.0",END)
+    master_secret = entry2.get()
 
-def buttonn():
-    pass
+    if len(title) == 0 or len(message) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="ERROR!",message="Please enter all info")
+    else:
+        #encryption
+        try:
+            with open("mySecret.txt","a") as data_file:
+                data_file.write(f"\n{title}\n{message}")
+        except FileNotFoundError:
+            with open("mySecret.txt","w") as data_file:
+                data_file.write(f"\n{title}\n{message}")
+        finally:
+            entry1.delete(first=0,last=END)
+            low_input.delete("0.0",END)
+            entry2.delete(first=0,last=END)
+
 
 photo = tkinter.PhotoImage(file="topSecret.png")
 photo_label = tkinter.Label(image=photo)
@@ -34,10 +49,10 @@ label4.pack()
 entry2 = tkinter.Entry(width=30)
 entry2.pack()
 
-button1 = tkinter.Button(text="Save&Encrypt",command=button)
+button1 = tkinter.Button(text="Save&Encrypt",command=save_and_encrypt_notes)
 button1.pack()
 
-button2 = tkinter.Button(text="Decrypt",command=buttonn)
+button2 = tkinter.Button(text="Decrypt")
 button2.pack()
 
 window.mainloop()
